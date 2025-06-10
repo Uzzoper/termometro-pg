ee4eac00c3937dab59336a05c6a0b0b61cb96834
Juan Peruzzo <juanperuzzo13@gmail.com>
2025-06-10 13:18:31 -0300

    fix:estrutura de pastas para vercel

---
+++ project/api/weather.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  const cityId = "3453186";
+  const apiKey = process.env.OPENWEATHER_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&units=metric&lang=pt_br`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      // Retorna o status e mensagem de erro para o front
+      const errorMessage = await response.text();
+      return res.status(response.status).json({ error: errorMessage });
+    }
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar clima' });
+  }
+}
